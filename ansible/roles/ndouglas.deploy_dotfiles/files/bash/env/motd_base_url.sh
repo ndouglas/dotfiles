85458b7c5cc80f9fe955f244e76ca00deeb6bc77
Nathan Douglas <github@tenesm.us>
2021-04-11 15:34:47 -0400

    Move MOTD Base URL into an environment variable.

---
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/env/motd_base_url.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+export MOTD_BASE_URL="https://raw.githubusercontent.com/ndouglas/asoiaf-noble-house-images/master/utf8/40";
--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/library/get_hostname_motd.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/library/get_hostname_motd.sh
@@ -4,6 +4,6 @@
 nd_get_hostname_motd() {
   : "${1?"Usage: ${FUNCNAME} HOSTNAME"}";
   the_hostname="${1}";
-  curl -L "https://raw.githubusercontent.com/ndouglas/asoiaf-noble-house-images/master/utf8/40/${the_hostname}.txt";
+  curl -L "${MOTD_BASE_URL}/${the_hostname}.txt";
 }
 
