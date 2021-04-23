8ce763d42456c54b7b7f43217aaed679e7a41026
Nathan Douglas <github@tenesm.us>
2021-04-23 12:30:02 -0400

    Slight restructuring of copy-bash-dotfiles to make deploys cleaner.

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/path/common.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/path/common.sh
@@ -3,10 +3,11 @@
 # A list of common paths that might not be in the PATH already.
 common_paths=(
   '/usr/local/sbin'
+  '~/python3/bin'
 )
 
 for common_path in "${common_paths[@]}"; do
   if [[ ! "${PATH}" =~ (^|:)"${common_path}"(:|$) ]] && [ -d "${common_path}" ]; then
-    PATH="${PATH}:${common_path}";
+    PATH="${common_path}:${PATH}";
   fi
 done
