e0e5a8129fb8ec0647e1a768baa0ec58cbf22002
Nathan Douglas <github@tenesm.us>
2021-04-23 20:28:24 -0400

    Adds update.sh (functionally).

--- update.sh
+++ update.sh
@@ -2,3 +2,26 @@
 
 # This is the local update script, expected to be executed from some machine
 # that already has the dotfiles installed and deployed.
+
+set -e;
+
+function nd_abort() {
+  : "${1?"Usage: ${FUNCNAME} MESSAGE"}";
+  message="${1}";
+  echo >&2 "${message}";
+  exit 1;
+}
+
+nd_check_work_tree() {
+  git update-index -q --ignore-submodules --refresh;
+  git diff-files --quiet --ignore-submodules -- || nd_abort "You have unstaged changes.  Please commit or stash them.";
+  git diff-index --cached --quiet HEAD --ignore-submodules -- || nd_abort "You have uncommitted changes.  Please commit or stash them.";
+}
+
+nd_update() {
+  nd_check_work_tree;
+  git pull;
+  ./deploy.sh;
+}
+
+nd_update;
