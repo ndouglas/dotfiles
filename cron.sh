111b839dd8d687a6a2a57a8619148dc6ff0b23c9
Nathan Douglas <github@tenesm.us>
2021-04-26 17:57:50 -0400

    Adds cron script.

---
+++ cron.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# A wrapper for update.sh that should be executed from cron.
+
+set -e;
+
+directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)";
+pushd "${directory}" > /dev/null;
+./update.sh > "${directory}"/.last_log;
+date > "${directory}"/.last_updated;
