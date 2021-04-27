40c02e2fd5eae336fbd8d82d9fe37c395b810310
Nathan Douglas <github@tenesm.us>
2021-04-27 17:36:53 -0400

    Work on dot_get_date_last_updated functions.

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/dotfiles/get_date_last_updated.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/dotfiles/get_date_last_updated.sh
@@ -6,6 +6,6 @@ dot_get_date_last_updated() {
   if [ -z "${host}" ]; then
     cat "${DOTFILES_ANSIBLE_PATH}"/../.last_updated;
   else
-    ssh "${host}" bash -lc 'cat ~/.dotfiles/.last_updated';
+    ssh "${host}" 'cat ~/.dotfiles/.last_updated';
   fi
 }
--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/dotfiles/get_date_last_updated_all.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/dotfiles/get_date_last_updated_all.sh
@@ -3,11 +3,13 @@
 # Get all hosts' date last updated.
 dot_get_date_last_updated_all() {
   authorized_key_path="${DOTFILES_ANSIBLE_PATH}"/roles/ndouglas.deploy_dotfiles/files/ssh/authorized_keys/;
-  dot_get_date_last_updated_all_file_handler() {
-    authorized_key="${1}";
-    host_at_hostname="$(basename "${authorized_key}" .pub)";
-    echo "${host_at_hostname}: $(dot_get_date_last_updated "${host_at_hostname}")";
-  }
-  export -f dot_get_date_last_updated_all_file_handler;
-  parallel dot_get_date_last_updated_all_file_handler ::: "${authorized_key_path}"/*.pub;
+  (
+    dot_get_date_last_updated_all_file_handler() {
+      authorized_key="${1}";
+      host_at_hostname="$(basename "${authorized_key}" .pub)";
+      echo -e "$(printf "%20s" "${host_at_hostname}"):\t$(ssh "${host_at_hostname}" 'cat ~/.dotfiles/.last_updated')";
+    }
+    export -f dot_get_date_last_updated_all_file_handler;
+    parallel dot_get_date_last_updated_all_file_handler ::: "${authorized_key_path}"/*.pub;
+  );
 }
