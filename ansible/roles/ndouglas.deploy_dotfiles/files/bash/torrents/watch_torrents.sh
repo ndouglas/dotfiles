12969c4e334243714dafe7eef5821c48b8365a78
Nathan Douglas <github@tenesm.us>
2021-05-08 17:50:52 -0400

    Light fixes.

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/torrents/watch_torrents.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/torrents/watch_torrents.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Given a watch directory, process any existing torrent files accordingly.
-torrents_watch_torrents() {
+torrent_watch_torrents() {
   : "${1?"Usage: ${FUNCNAME} DIRECTORY"}";
   torrent_directory="${1}";
   for torrent_file in "${torrent_directory}"/*.torrent; do
