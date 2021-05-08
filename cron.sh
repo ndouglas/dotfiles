5f144ecfc5c3d50eed7feffcdd8bcbc18bbd62bf
Nathan Douglas <github@tenesm.us>
2021-05-08 18:07:54 -0400

    Fetch first!

--- cron.sh
+++ cron.sh
@@ -6,6 +6,7 @@ set -e;
 
 directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)";
 pushd "${directory}" > /dev/null;
+git fetch --quiet;
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
   ./update.sh > "${directory}"/.last_log;
   date +'%Y-%m-%d %H:%M:%S' > "${directory}"/.last_updated;
