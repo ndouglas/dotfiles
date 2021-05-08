05c517122241198d9c2900d80e0499de9bee994e
Nathan Douglas <github@tenesm.us>
2021-05-08 17:58:54 -0400

    Improve torrent_watch_torrents function.

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/torrents/watch_torrents.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/torrents/watch_torrents.sh
@@ -4,7 +4,12 @@
 torrent_watch_torrents() {
   : "${1?"Usage: ${FUNCNAME} DIRECTORY"}";
   torrent_directory="${1}";
-  for torrent_file in "${torrent_directory}"/*.torrent; do
-    torrent_assign_torrent "${torrent_file}";
-  done;
+  if test -n "$(shopt -s nullglob; echo "${torrent_directory}"/*.torrent)"; then
+    for torrent_file in "${torrent_directory}"/*.torrent; do
+      echo "${torrent_file}";
+      if [ -f "${torrent_file}" ]; then
+        torrent_assign_torrent "${torrent_file}";
+      fi;
+    done;
+  fi;
 }
