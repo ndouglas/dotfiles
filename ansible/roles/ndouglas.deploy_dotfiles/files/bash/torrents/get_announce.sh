866018d3d75f52dc1073639a64c6a9549d5f88d6
Nathan Douglas <github@tenesm.us>
2021-05-08 12:59:35 -0400

    Expand torrent-parsing functions a bit.

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/torrents/get_announce.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/torrents/get_announce.sh
@@ -1,6 +1,6 @@
  #!/usr/bin/env bash
 
-# Retrieve the tracker of a specified torrent file.
+# Retrieve the announce URL of a specified torrent file.
 torrent_get_announce() {
   : "${1?"Usage: ${FUNCNAME} FILE"}";
   torrent_file="${1}"
