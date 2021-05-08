8b6943a7c0b7145f2719642267d6dd8c0de70cbc
Nathan Douglas <github@tenesm.us>
2021-05-08 17:37:04 -0400

    Domain replacements.

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/torrents/get_safe_announce_domain.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Retrieve the "safe" (alphanumeric) domain of the announce URL of a specified torrent file.
-torrent_get_safe_announce_domain() {
-  : "${1?"Usage: ${FUNCNAME} TORRENT_FILE"}";
-  torrent_file="${1}";
-  torrent_get_announce_domain "${torrent_file}" | tr -cd '[:alnum:]';
-}
