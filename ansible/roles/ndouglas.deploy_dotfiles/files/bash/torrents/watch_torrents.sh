d4d4458abf9449fa7eae8c5ba8a86331932d8465
Nathan Douglas <github@tenesm.us>
2021-05-08 17:22:18 -0400

    Might be nice to actually execute the code I just wrote.

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/torrents/watch_torrents.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/torrents/watch_torrents.sh
@@ -5,6 +5,6 @@ torrents_watch_torrents() {
   : "${1?"Usage: ${FUNCNAME} DIRECTORY"}";
   torrent_directory="${1}";
   for torrent_file in "${torrent_directory}"/*.torrent; do
-    echo "${torrent_File}" >&2;
+    torrent_assign_torrent "${torrent_file}";
   done;
 }
