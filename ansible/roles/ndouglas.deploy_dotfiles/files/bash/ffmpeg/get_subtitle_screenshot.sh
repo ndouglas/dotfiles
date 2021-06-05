69ab2b83af53918e1b0d75e5d10f5b9b700f905c
Nathan Douglas <github@tenesm.us>
2021-06-04 21:15:14 -0400

    Save screenshot to temporary directory.

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/ffmpeg/get_subtitle_screenshot.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/ffmpeg/get_subtitle_screenshot.sh
@@ -6,15 +6,17 @@ ffmpeg_get_subtitle_screenshot() {
   video_file="${1}";
   expression="${2}";
   subtitle_file="${video_file}.srt";
+  destination_path="$(mktemp -d)";
   timestamp="$(ffmpeg_get_subtitle_timestamp "${video_file}" "${expression}")";
   full_line="$(ffmpeg_get_subtitle_line "${video_file}" "${expression}")";
   filename="$(echo "${timestamp}_${full_line}" | sed -e 's/[^A-Za-z0-9_]/_/g' -e 's/_\+/_/g').png";
+  full_path="${destination_path}/${filename}";
   ffmpeg \
     -ss "${timestamp}" \
     -copyts \
     -i "${video_file}" \
     -vf subtitles="${subtitle_file}" \
     -vframes 1 \
-    "${filename}";
-  echo "${filename}";
+    "${full_path}";
+  echo "${full_path}";
 }
