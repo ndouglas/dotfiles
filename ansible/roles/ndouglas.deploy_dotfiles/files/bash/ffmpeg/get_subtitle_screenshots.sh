d76bfe5f26e0d799cb3e1c3281e7c7f59c8bf2e8
Nathan Douglas <github@tenesm.us>
2021-06-05 12:22:28 -0400

    More work on ffmpeg stuff.

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/ffmpeg/get_subtitle_screenshots.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/ffmpeg/get_subtitle_screenshots.sh
@@ -9,19 +9,26 @@ ffmpeg_get_subtitle_screenshots() {
   destination_path="$(mktemp -d)";
   timestamps="$(ffmpeg_get_subtitle_timestamps "${video_file}" "${expression}")";
   [ "$?" -eq 0 ] || return -1;
-  while read -r timestamp; do
-    full_line="$(ffmpeg_get_subtitle_lines_at_timestamp "${video_file}" "${timestamp}")";
-    filename="$(echo "${timestamp}_${full_line}" | sed -e 's/[^A-Za-z0-9_]/_/g' -e 's/_\+/_/g').png";
-    full_path="${destination_path}/${filename}";
-    ffmpeg \
-      -hide_banner \
-      -loglevel error \
-      -ss "${timestamp}" \
-      -copyts \
-      -i "${video_file}" \
-      -vf subtitles="${subtitle_file}" \
-      -vframes 1 \
-      "${full_path}";
-    echo "${full_path}";
-  done < <(printf '%s\n' "${timestamps}");
+  ifs="${IFS}";
+  IFS=$'\n'
+  for timestamp in $timestamps; do
+    if [[ "${timestamp}" =~ ^[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}$ ]]; then
+      full_line="$(ffmpeg_get_subtitle_lines_at_timestamp "${video_file}" "${timestamp}")";
+      filename="$(echo "${timestamp}_${full_line}" | sed -e 's/[^A-Za-z0-9_]/_/g' -e 's/_\+/_/g').png";
+      full_path="${destination_path}/${filename}";
+      ffmpeg \
+        -hide_banner \
+        -loglevel error \
+        -ss "${timestamp}" \
+        -copyts \
+        -i "${video_file}" \
+        -vf subtitles="${subtitle_file}" \
+        -vframes 1 \
+        "${full_path}";
+      echo "${full_path}";
+    else
+      echo "Timestamp ${timestamp} does not match expected format." >&2;
+    fi;
+  done;
+  IFS=ifs;
 }
