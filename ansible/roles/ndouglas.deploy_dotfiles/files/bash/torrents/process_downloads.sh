89390d3019e5b43859f550e549d11811d61d7da8
Nathan Douglas <github@tenesm.us>
2021-07-16 17:22:28 -0400

    Remove some debugging stuff.

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/torrents/process_downloads.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/torrents/process_downloads.sh
@@ -7,12 +7,9 @@ torrent_process_downloads() {
   find "${downloads_directory}" -mindepth 1 -maxdepth 1 -type d -print0 \
     | while read -d $'\0' the_directory; do
         process_script="${the_directory}/process.sh";
-        echo "Looking for ${process_script} ...";
         if [ -f "${process_script}" ]; then
-          echo "Looking for files to feed to ${process_script} ...";
           find "${the_directory}" -mindepth 1 -maxdepth 1 -mmin +5 ! -path "${process_script}" -print0 \
             | while read -d $'\0' the_item; do
-                echo "Feeding ${the_item} to ${process_script} ...";
                 "${process_script}" "${the_item}";
               done;
         fi;
