aa77e151b41810b0a0538b269dcf36ea80d8da01
Nathan Douglas <github@tenesm.us>
2021-09-14 16:14:02 -0400

    Does things to eliminate spurious changed states.

---
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/proxmox/remove_host.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Remove a Proxmox VE host.
+pve_remove_host() {
+  : "${1?"Usage: ${FUNCNAME[0]} HOST"}";
+  host="${1}";
+  pushd "${DOTFILES_ANSIBLE_PATH}" >> /dev/null;
+  ANSIBLE_GATHERING=no ans_run_role_task "${host}" 'ndouglas.proxmox_host' 'remove_pve_host.yaml';
+  popd > /dev/null;
+}
