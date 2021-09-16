ae67254205a3b2a7cec4f4a8f0f635bd0e495dd2
Nathan Douglas <github@tenesm.us>
2021-09-16 09:53:51 -0400

    Kubernetes stuff.

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/proxmox/k8s_create_cluster.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/proxmox/k8s_create_cluster.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Create a Proxmox VE kubernetes cluster.
-pve_create_k8s() {
-  : "${1?"Usage: ${FUNCNAME[0]} HOST"}";
+pve_k8s_create_cluster() {
+  : "${1?"Usage: ${FUNCNAME[0]} CLUSTER_NAME"}";
   host="${1}";
   pushd "${DOTFILES_ANSIBLE_PATH}" >> /dev/null;
   ANSIBLE_GATHERING=no ans_run_role_task "${host}" 'ndouglas.proxmox_k8s' 'create_cluster.yaml';
