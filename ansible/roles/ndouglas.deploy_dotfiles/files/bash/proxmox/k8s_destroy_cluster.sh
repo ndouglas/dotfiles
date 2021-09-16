a5e1bd4c14b8080ee1d3d688fb9a7008311a0a65
Nathan Douglas <github@tenesm.us>
2021-09-16 13:30:40 -0400

    Remove dotfiles_home variable, remove Python virtualenv stuff, more k8s work.

---
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/proxmox/k8s_destroy_cluster.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Destroy a Proxmox VE kubernetes cluster.
+pve_k8s_destroy_cluster() {
+  : "${1?"Usage: ${FUNCNAME[0]} CLUSTER_NAME"}";
+  host="${1}";
+  pushd "${DOTFILES_ANSIBLE_PATH}" >> /dev/null;
+  ANSIBLE_GATHERING=no ans_run_role_task "${host}" 'ndouglas.proxmox_k8s' 'destroy_cluster.yaml';
+  popd > /dev/null;
+}
