0bdd62fc1d386378bed056ff25988b7d04366a05
Nathan Douglas <github@tenesm.us>
2021-09-17 13:02:49 -0400

    Refines dot_get_date_last_checked_all().

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/dotfiles/get_date_last_checked_all.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/dotfiles/get_date_last_checked_all.sh
@@ -6,8 +6,11 @@ dot_get_date_last_checked_all() {
   (
     dot_get_date_last_checked_all_file_handler() {
       authorized_key="${1}";
-      host_at_hostname="$(basename "${authorized_key}" .pub)";
-      echo -e "$(printf "%20s" "${host_at_hostname}"):\t$(ssh "${host_at_hostname}" 'cat ~/.dotfiles/.last_checked')";
+      user_at_hostname="$(basename "${authorized_key}" .pub)";
+      user="$(echo "${user_at_hostname}" | cut -d@ -f1)";
+      hostname="$(echo "${user_at_hostname}" | cut -d@ -f2)";
+      prompt="$(bash -lc nd_preview_hostname_prompt "${hostname}" "${user}" '~')";
+      echo -e "$(printf "%20s" "${prompt}"):\t$(ssh "${user_at_hostname}" 'cat ~/.dotfiles/.last_checked')";
     }
     export -f dot_get_date_last_checked_all_file_handler;
     parallel dot_get_date_last_checked_all_file_handler ::: "${authorized_key_path}"/*.pub;
