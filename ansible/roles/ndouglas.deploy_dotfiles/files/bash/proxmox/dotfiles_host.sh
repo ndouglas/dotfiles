6dfbff0a8ef8074d95e22e9c631e260f54afe2c9
Nathan Douglas <github@tenesm.us>
2021-09-17 07:10:42 -0400

    Fixing some apparently deprecated code.

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/proxmox/dotfiles_host.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/proxmox/dotfiles_host.sh
@@ -5,6 +5,6 @@ pve_dotfiles_host() {
   : "${1?"Usage: ${FUNCNAME[0]} HOST"}";
   host="${1}";
   pushd "${DOTFILES_ANSIBLE_PATH}" >> /dev/null;
-  ANSIBLE_GATHERING=no ans_run_role_task "${host}" 'ndouglas.proxmox_host' 'dotfiles_pve_host.yaml';
+  ANSIBLE_GATHERING=explicit ans_run_role_task "${host}" 'ndouglas.proxmox_host' 'dotfiles_pve_host.yaml';
   popd > /dev/null;
 }
--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/proxmox/k8s_destroy_cluster.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/proxmox/k8s_destroy_cluster.sh
@@ -5,6 +5,6 @@ pve_k8s_destroy_cluster() {
   : "${1?"Usage: ${FUNCNAME[0]} CLUSTER_NAME"}";
   host="${1}";
   pushd "${DOTFILES_ANSIBLE_PATH}" >> /dev/null;
-  ANSIBLE_GATHERING=no ans_run_role_task "${host}" 'ndouglas.proxmox_k8s' 'destroy_cluster.yaml';
+  ANSIBLE_GATHERING=explicit ans_run_role_task "${host}" 'ndouglas.proxmox_k8s' 'destroy_cluster.yaml';
   popd > /dev/null;
 }
