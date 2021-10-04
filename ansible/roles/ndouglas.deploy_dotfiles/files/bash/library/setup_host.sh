993c1339d3d0ba1600ed46f642e4ff7b0fdf8577
Nathan Douglas <github@tenesm.us>
2021-10-03 21:00:25 -0400

    WIP on things.

--- ansible/roles/ndouglas.deploy_dotfiles/files/bash/library/setup_host.sh
+++ ansible/roles/ndouglas.deploy_dotfiles/files/bash/library/setup_host.sh
@@ -5,6 +5,6 @@ nd_setup_host() {
   : "${1?"Usage: ${FUNCNAME[0]} HOST"}";
   host="${1}";
   pushd "${DOTFILES_ANSIBLE_PATH}" >> /dev/null;
-  ANSIBLE_GATHERING=explicit ans_run_role "${host}" 'ndouglas.setup_host';
+  ans_run_role "${host}" 'ndouglas.setup_host' -e 'ansible_ssh_user=root';
   popd > /dev/null;
 }
